68b54fe29d2726f9465eb922561fb08bf229ec06
doug <dug@sorted.net>
2019-01-05 16:50:52 +1100

    can start vault in dev mode with either version 1 or 2 or kv

--- startDevVault.sh
+++ startDevVault.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -eux
 
 vault server --dev --dev-root-token-id="00000000-0000-0000-0000-000000000000" 
+
+if [[ $# -eq 1 ]]; then
+  $KV_VERSION=$1
+  echo "Switch to kv version ${KV_VERSION}"
+  vault secrets disable secret
+  vault secrets enable -version=${KV_VERSION} -path=secret -description='local secrets' kv
+fi
